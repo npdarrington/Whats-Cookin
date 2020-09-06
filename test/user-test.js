d089c78fa36f9141ade995e3b2f1d2ec36c3fc23
Nathan Darrington <npdarrington@icloud.com>
2020-09-06 13:47:45 -0600

    Fixed bug in user test routing to wrong file directory for users-data

--- test/user-test.js
+++ test/user-test.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 
 import User from '../src/user';
-import data from '../data/users-data';
+import data from '../src/data/users-data';
 
 describe('User', function() {
   let user;
