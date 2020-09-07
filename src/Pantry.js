8045d1036546d22450cdc6be5ef4c40b8eed5244
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-09-06 20:32:29 -0600

    Create getPantryItems method in Pantry class

--- src/Pantry.js
+++ src/Pantry.js
@@ -1,9 +1,30 @@
 class Pantry {
-  constructor() {
-
+  constructor(userPantry = []) { //userPantry which is an array?
+    this.userPantry = userPantry;
+  }
+  getPantryItems(user) {
+    return user.pantry;
+  }
+  
+  findPantryInfo() {
+    user.pantry.forEach(item => {
+      let itemInfo = ingredientsData.find(ingredient => {
+        return ingredient.id === item.ingredient;
+      });
+      let originalIngredient = pantryInfo.find(ingredient => {
+        if (itemInfo) {
+          return ingredient.name === itemInfo.name;
+        }
+      });
+      if (itemInfo && originalIngredient) {
+        originalIngredient.count += item.amount;
+      } else if (itemInfo) {
+        pantryInfo.push({name: itemInfo.name, count: item.amount});
+      }
+    });
+    displayPantryInfo(pantryInfo.sort((a, b) => a.name.localeCompare(b.name)));
   }
 }
 
-if (typeof module !== 'undefined') {
-  module.exports = Pantry;
-}
+
+module.exports = Pantry;
