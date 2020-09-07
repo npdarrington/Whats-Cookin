fc4b0aceaf949bc01235dfc1f6b2900b71ee337a
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-09-06 20:00:40 -0600

    Set up initial Pantry test file

--- test/Pantry-test.js
+++ test/Pantry-test.js
@@ -1,5 +1,10 @@
 import { expect } from 'chai';
+
+import users from '../src/data/users-data.js'
+import recipeData from '../src/data/recipe-data.js'
+import User from '../src/user.js';
 import Pantry from '../src/Pantry.js';
+import Recipe from '../src/recipe.js';
 
 describe.only('Pantry', () => {
   let pantry;
