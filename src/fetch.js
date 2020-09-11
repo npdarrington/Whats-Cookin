8f5436e38673b6d8d1566937b309b7773800e4b7
Jeff Woltjen <jeff.woltjen@gmail.com>
2020-09-11 13:31:37 -0500

    Build recipeData variable to store retrieved recipe data from server.

--- src/fetch.js
+++ src/fetch.js
@@ -6,5 +6,12 @@ function fetchData() {
       return data.ingredientsData;
     })
     .catch(err => console.log(err.message))
+
+  let recipeData = fetch('https://fe-apps.herokuapp.com/api/v1/whats-cookin/1911/ingredients/ingredientsData');
+    .then(response => response.json())
+    .then(data => {
+      return data.recipesData;
+    })
+  .catch(err => console.log(err.message))
 }
 export default fetchData;
