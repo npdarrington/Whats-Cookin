db1084323d13a9312105d31f530cc6b53eaa9fc2
Jeff Woltjen <jeff.woltjen@gmail.com>
2020-09-13 14:12:03 -0500

    Format fetch.js in conformance with formatting paradigms.

--- src/fetch.js
+++ src/fetch.js
@@ -1,22 +1,28 @@
 
 
-  const getIngredientsData = fetch('https://fe-apps.herokuapp.com/api/v1/whats-cookin/1911/ingredients/ingredientsData')
+const getIngredientsData = () => {
+  return fetch('https://fe-apps.herokuapp.com/api/v1/whats-cookin/1911/ingredients/ingredientsData')
     .then(response => response.json())
     .then(data => {
       return data.ingredientsData;
     })
     .catch(err => console.log(err.message))
+}
 
-  const getRecipeData = fetch('https://fe-apps.herokuapp.com/api/v1/whats-cookin/1911/recipes/recipeData')
+const getRecipeData = () => {
+  return fetch('https://fe-apps.herokuapp.com/api/v1/whats-cookin/1911/recipes/recipeData')
     .then(response => response.json())
     .then(data => {
       return data.recipeData;
     })
     .catch(err => console.log(err.message))
+}
 
-  const getUserData = fetch('https://fe-apps.herokuapp.com/api/v1/whats-cookin/1911/users/wcUsersData')
+const getUserData = () => {
+  return fetch('https://fe-apps.herokuapp.com/api/v1/whats-cookin/1911/users/wcUsersData')
     .then(response => response.json())
     .then(data => {
       return data.wcUsersData
     })
     .catch(err => console.log(err.message))
+}
