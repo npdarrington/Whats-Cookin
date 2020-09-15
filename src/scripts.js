483c3d6f57600686cc67e2d5383acb1413693a43
Hanna Kim <hannakim91@gmail.com>
2020-09-15 15:16:01 -0600

    Transplant functions to create recipe instructions on DOM to domUpdates object

--- src/scripts.js
+++ src/scripts.js
@@ -237,55 +237,55 @@ function showSavedRecipes() {
 }
 
 // CREATE RECIPE INSTRUCTIONS
-function openRecipeInfo(event) {
-  fullRecipeInfo.style.display = "inline";
-  let recipeId = event.path.find(e => e.id).id;
-  let recipe = recipes.find(recipe => recipe.id === Number(recipeId));
-  generateRecipeTitle(recipe, generateIngredients(recipe));
-  addRecipeImage(recipe);
-  generateInstructions(recipe);
-  fullRecipeInfo.insertAdjacentHTML("beforebegin", "<section id='overlay'></section>");
-}
+// function openRecipeInfo(event) {
+//   fullRecipeInfo.style.display = "inline";
+//   let recipeId = event.path.find(e => e.id).id;
+//   let recipe = recipes.find(recipe => recipe.id === Number(recipeId));
+//   generateRecipeTitle(recipe, generateIngredients(recipe));
+//   addRecipeImage(recipe);
+//   generateInstructions(recipe);
+//   fullRecipeInfo.insertAdjacentHTML("beforebegin", "<section id='overlay'></section>");
+// }
 
-function generateRecipeTitle(recipe, ingredients) {
-  let recipeTitle = `
-    <button id="exit-recipe-btn">X</button>
-    <h3 id="recipe-title">${recipe.name}</h3>
-    <h4>Ingredients</h4>
-    <p>${ingredients}</p>`
-  fullRecipeInfo.insertAdjacentHTML("beforeend", recipeTitle);
-}
+// function generateRecipeTitle(recipe, ingredients) {
+//   let recipeTitle = `
+//     <button id="exit-recipe-btn">X</button>
+//     <h3 id="recipe-title">${recipe.name}</h3>
+//     <h4>Ingredients</h4>
+//     <p>${ingredients}</p>`
+//   fullRecipeInfo.insertAdjacentHTML("beforeend", recipeTitle);
+// }
 
-function addRecipeImage(recipe) {
-  document.getElementById("recipe-title").style.backgroundImage = `url(${recipe.image})`;
-}
+// function addRecipeImage(recipe) {
+//   document.getElementById("recipe-title").style.backgroundImage = `url(${recipe.image})`;
+// }
 
-// ********
-function generateIngredients(recipe) {
-  const mappedRecipe = recipe.ingredients.map(recipeIngredient => {
-    ingredientsData.forEach(ingredient => {
-      if (ingredient.id === recipeIngredient.id) {
-        recipeIngredient.name = ingredient.name
-      }
-    })
-    return recipeIngredient
-  })
-  return recipe && mappedRecipe.map(i => {
-    return `${capitalize(i.name)} (${i.quantity.amount} ${i.quantity.unit})`
-  }).join(", ");
-}
+// // ********
+// function generateIngredients(recipe) {
+//   const mappedRecipe = recipe.ingredients.map(recipeIngredient => {
+//     ingredientsData.forEach(ingredient => {
+//       if (ingredient.id === recipeIngredient.id) {
+//         recipeIngredient.name = ingredient.name
+//       }
+//     })
+//     return recipeIngredient
+//   })
+//   return recipe && mappedRecipe.map(i => {
+//     return `${capitalize(i.name)} (${i.quantity.amount} ${i.quantity.unit})`
+//   }).join(", ");
+// }
 
-function generateInstructions(recipe) {
-  let instructionsList = "";
-  let instructions = recipe.instructions.map(i => {
-    return i.instruction
-  });
-  instructions.forEach(i => {
-    instructionsList += `<li>${i}</li>`
-  });
-  fullRecipeInfo.insertAdjacentHTML("beforeend", "<h4>Instructions</h4>");
-  fullRecipeInfo.insertAdjacentHTML("beforeend", `<ol>${instructionsList}</ol>`);
-}
+// function generateInstructions(recipe) {
+//   let instructionsList = "";
+//   let instructions = recipe.instructions.map(i => {
+//     return i.instruction
+//   });
+//   instructions.forEach(i => {
+//     instructionsList += `<li>${i}</li>`
+//   });
+//   fullRecipeInfo.insertAdjacentHTML("beforeend", "<h4>Instructions</h4>");
+//   fullRecipeInfo.insertAdjacentHTML("beforeend", `<ol>${instructionsList}</ol>`);
+// }
 
 function exitRecipe() {
   while (fullRecipeInfo.firstChild &&
