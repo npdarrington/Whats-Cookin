8a481eed3a8577b88efac35409580403b340044d
Hanna Kim <hannakim91@gmail.com>
2020-09-15 20:14:55 -0600

    Finish writing first chai spies test fom domUpdates

--- test/domUpdates-test.js
+++ test/domUpdates-test.js
@@ -2,30 +2,27 @@ const chai = require('chai');
 const expect = chai.expect;
 const spies = require('chai-spies');
 import domUpdates from '../src/domUpdates';
-import Recipe from '../src/recipe';
+// import Recipe from '../src/recipe';
 import data from '../src/data/recipe-data';
 
 chai.use(spies)
 
 describe ('DOM Test', () => {
 
-  let recipe;
+  
   beforeEach(() => {
-    recipe = new Recipe(data[0])
-    domUpdates.assignRecipeData(recipe)
-    global.document = {}
-    chai.spy.on(document, ['querySelector', 'querySelectorAll'], () => {
-      return { innerText: '' }
-    });
+    domUpdates.assignRecipeData(data)
+    global.domUpdates = {}
+    chai.spy.on(domUpdates, ['displayRecipeDetails'], () => {});
   });
 
   afterEach(() => {
     chai.spy.restore(domUpdates);
-  });
+  }); 
 
-  it('should change the innerText of html element', () => {
-    domUpdates.displayRecipeDetails()
-    expect(1).to.equal(1)
-  })
+  it('should check if displayRecipeDetails is called when creating recipe cards', () => {
+    domUpdates.createCards()
+    expect(domUpdates.displayRecipeDetails).to.have.been.called(50)
+  });
   
 });
